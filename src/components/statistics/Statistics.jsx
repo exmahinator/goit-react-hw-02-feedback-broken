368106c83a0ba997736a543b787365e69b9623ef
Yaroslav Drohoziuk <exmahinator@ukr.net>
2022-09-25 12:06:55 +0300

    Update Statistics.jsx

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FeedbackItem } from 'components/ui';
+import PropTypes from 'prop-types';
 
 function Statistics({ good, neutral, bad, total, positivePercentage }) {
   return (
@@ -16,3 +17,11 @@ function Statistics({ good, neutral, bad, total, positivePercentage }) {
 }
 
 export default Statistics;
+
+Statistics.PropTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
+};
